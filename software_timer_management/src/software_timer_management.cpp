34102886b5fc6a531ac0bc5daaf123216cc884b4
tom <tomcmead0@gmail.com>
2023-12-21 19:57:53 +0000

    Print timer info in callback and stop timer after 5 calls in software timer management

--- software_timer_management/src/software_timer_management.cpp
+++ software_timer_management/src/software_timer_management.cpp
@@ -9,37 +9,30 @@
 #include <timers.h>
 
 /*----------------------------------------------------------------------------
-  global variables
-----------------------------------------------------------------------------*/
-static unsigned long ulCallCount = 0;
-
-/*----------------------------------------------------------------------------
   public functions
 ----------------------------------------------------------------------------*/
-/*************************************************************************//**
-* @brief One-shot timer will execute its callback function once only.
-*        A one-shot timer can be restarted manually, but will not restart itself.
-* @param TimerHandle_t xTimer timer handler
-* @return None
-*****************************************************************************/
-static void prvOneShotTimerCallback(TimerHandle_t xTimer){
-    TickType_t xTimeNow = xTaskGetTickCount();
-    Serial.print("One-shot timer callback executing: ");
-    Serial.println(xTimeNow);
-    ulCallCount++;
-}
-
 /*************************************************************************//**
 * @brief Auto-reload timer will re-start itself each time it expires, 
 *        resulting in periodic execution of its callback function.
 * @param TimerHandle_t xTimer timer handler
 * @return None
 *****************************************************************************/
-static void prvAutoReloadTimerCallback(TimerHandle_t xTimer){
-    TickType_t xTimeNow = xTaskGetTickCount();
-    Serial.print("Auto-reload timer callback executing: ");
-    Serial.println(xTimeNow);
-    ulCallCount++;
+static void prvTimerCallback(TimerHandle_t xTimer){
+    static uint32_t ulExecutionCount = 0;
+    ulExecutionCount++;
+
+    uint32_t ulTimerID = (uint32_t) pvTimerGetTimerID(xTimer);
+    const char *pcTimerName = pcTimerGetName(xTimer);
+
+    Serial.print(pcTimerName);
+    Serial.print(" ");
+    Serial.print(ulTimerID);
+    Serial.print(" callback: ");
+    Serial.println(ulExecutionCount);
+
+    if(ulExecutionCount == 5){
+        xTimerStop(xTimer, 0);
+    }
 }
 
 int main( void ){
@@ -50,20 +43,19 @@ int main( void ){
 
     // One shot timer - does not automatically reset itself, so will only expire
     // and therefore execute its callback once unless it is manually restarted
-    xOneShotTimer = xTimerCreate("OneShot",                 //const char * const pcTimerName
-                               ONE_SHOT_TIMER_PERIOD, // TickType_t xTimerPeriodInTicks
-                               pdFALSE,                   // UBaseType_t uxAutoReload (one shot)
-                               0,                         // void * pvTimerID (no timer id)
-                               prvOneShotTimerCallback);  // TimerCallbackFunction_t pxCallbackFunction
+    xOneShotTimer = xTimerCreate("OneShot",             //const char * const pcTimerName
+                                 ONE_SHOT_TIMER_PERIOD, // TickType_t xTimerPeriodInTicks
+                                 pdFALSE,               // UBaseType_t uxAutoReload (one shot)
+                                 0,                     // void * pvTimerID (no timer id)
+                                 prvTimerCallback);     // TimerCallbackFunction_t pxCallbackFunction
 
     // Auto reload timer - resets itself each time it expires, causing the timer to expire
     // and therefore execute its callback periodically
-    xAutoReloadTimer = xTimerCreate("AutoReload",                 //const char * const pcTimerName
-                               AUTO_RELOAD_TIMER_PERIOD, // TickType_t xTimerPeriodInTicks
-                               pdTRUE,                       // UBaseType_t uxAutoReload (auto reload)
-                               0,                            // void * pvTimerID (no timer id)
-                               prvAutoReloadTimerCallback);  // TimerCallbackFunction_t pxCallbackFunction
-
+    xAutoReloadTimer = xTimerCreate("AutoReload",             //const char * const pcTimerName
+                                    AUTO_RELOAD_TIMER_PERIOD, // TickType_t xTimerPeriodInTicks
+                                    pdTRUE,                   // UBaseType_t uxAutoReload (auto reload)
+                                    0,                        // void * pvTimerID (no timer id)
+                                    prvTimerCallback);        // TimerCallbackFunction_t pxCallbackFunction
 
     if((xOneShotTimer != NULL) && (xAutoReloadTimer != NULL)){
         xTimer1Started = xTimerStart(xOneShotTimer, // TimerHandle_t xTimer
